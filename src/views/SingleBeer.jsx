b9e923b24915181fb7f2508e7f18d86d055dfca1
Victor Perdiguer <victor.perdiguer@gmail.com>
2023-03-21 10:40:12 +0100

    iteration 3 done

--- src/views/SingleBeer.jsx
+++ src/views/SingleBeer.jsx
@@ -1,13 +1,36 @@
 import React from "react"
+import { useState, useEffect } from "react";
 import beerService from "../services/beerService";
 import { useParams } from "react-router-dom";
 
 const SingleBeer = () => {
-    const beerId = useParams()
-    const getBeer 
+    const [beer, setBeer] = useState({})
+    const {beerId} = useParams()
+    const getBeer = async () => {
+        const response = await beerService.getBeerById(beerId);
+        console.log(response);
+        setBeer(response);
+    }
+    useEffect(() => {
+        getBeer();
+    }, [])
+
     return (
-        <div>
-        
+        <div className="beer-detail">
+            <div className="detail-container">
+                <img src={beer.image_url} alt={beer.name} />
+            </div>
+            <div className="detail-container">
+                <h1 className="beer-name">{beer.name}</h1>
+                <h2 className="beer-attenuation">{beer.attenuation_level}</h2>
+            </div>
+            <div className="detail-container">
+                <h1 className="beer-tagline">{beer.tagline}</h1>
+                <h2 className="beer-date">{beer.first_brewed}</h2>
+            </div>
+            <p>{beer.description}</p>
+            <h3>{beer.contributed_by}</h3>
+
         </div>
     )
 };
